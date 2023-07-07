84e4362b2722936252f9620e28e5f759ad3c2909
LuongLuyen <luongvanluyen2003@gmail.com>
2023-07-07 15:25:46 +0700

    complete admin page and web page views

--- src/main/java/com/javaweb/controller/web/HomeController.java
+++ src/main/java/com/javaweb/controller/web/HomeController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.javaweb.model.UserModel;
 
 @WebServlet(urlPatterns = {"/trang-chu"})
 public class HomeController extends HttpServlet {
@@ -22,9 +21,6 @@ public class HomeController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		UserModel userModel = new UserModel();
-		userModel.setFullName("data from model: fullName Nguyen Van A");
-		request.setAttribute("model", userModel);
 		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
 		rd.forward(request, response);
 
