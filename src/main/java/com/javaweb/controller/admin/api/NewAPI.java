b89d4e787f86582e099477ebeedca0f05bafbe70
LuongLuyen <luongvanluyen2003@gmail.com>
2023-07-12 07:57:56 +0700

    Restful api web service

---
+++ src/main/java/com/javaweb/controller/admin/api/NewAPI.java
@@ -0,0 +1,34 @@
+package com.javaweb.controller.admin.api;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/api-admin-new"})
+public class NewAPI extends HttpServlet {
+
+	private static final long serialVersionUID = -6922552214992789789L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
